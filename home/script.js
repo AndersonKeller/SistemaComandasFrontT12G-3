3a2c46fd8a0152044b7138104cd7fd3b1cbacd82
Artur Luis <arturluis0706@hotmail.com>
2024-09-25 19:53:30 -0300

    Iniciado as interações com os botões e alterado alguns detalhes no css NA TELA HOME

---
+++ home/script.js
@@ -0,0 +1,8 @@
+const comandas = document.getElementById("comandas")
+const usuarios = document.getElementById("usuarios")
+const pedidoCozinha = document.getElementById("pedidoCozinha")
+const cardapio = document.getElementById("cardapio")
+
+cardapio.addEventListener("click", () => {
+    window.location.href = "/cardapio/index.html"
+})
