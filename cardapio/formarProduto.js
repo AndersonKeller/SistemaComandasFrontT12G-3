414575f97cb9de822dbb7d5ad1bf726c0817cfda
KAUÃDEOLIVEIRALANGE <kauaoliveiralange9580@gmail.com>
2024-09-26 21:55:21 -0300

    criação do modal de comandas

---
+++ cardapio/formarProduto.js
@@ -0,0 +1,31 @@
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+export async function formarProduto(seletor){
+    const cardapio = await fetch("https://localhost:7125/api/CardapioItems",{
+        headers:headers
+    })
+
+
+    const ItensCardapio = await cardapio.json()
+    const items = document.querySelector(seletor)
+    console.log(ItensCardapio,"response await")
+    console.log(ItensCardapio)
+
+    ItensCardapio.forEach(async(item,index)=>{
+        items.insertAdjacentHTML("beforeend",`
+         
+           
+            <li>
+                <p class="item-name">${item.titulo}</p>
+                <p class="item-desc">${item.descricao}</p>
+                <p class="item-price">R$${item.preco}</p>
+                <button class="add-item">+</button>
+            </li>
+           
+        
+            `)}
+        )
+    }
--- cardapio/script.js
+++ cardapio/script.js
@@ -1,32 +1,6 @@
-const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-}
-
-async function formarProduto(){
-    const cardapio = await fetch("https://localhost:7204/api/CardapioItems",{
-        headers:headers,
-    })
+import {formarProduto} from "./formarProduto.js"
 
 
-    const ItensCardapio = await cardapio.json()
-    const items = document.querySelector("#items")
-    console.log(ItensCardapio,"response await")
-    console.log(ItensCardapio)
 
-    ItensCardapio.forEach(async(item,index)=>{
-        items.insertAdjacentHTML("beforeend",`
-         
-           
-            <li>
-                <p class="item-name">${item.titulo}</p>
-                <p class="item-desc">${item.descricao}</p>
-                <p class="item-price">R$${item.preco}</p>
-                <button class="add-item">+</button>
-            </li>
-           
-        
-            `)}
-        )
-    }
-formarProduto()
+
+formarProduto("#items")
