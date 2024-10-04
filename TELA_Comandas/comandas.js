f64883ae64f1081f244221e8e34269214a01655e
Kaua_lange <kauaoliveiralange9580@gmail.com>
2024-10-03 21:58:01 -0300

    Adição do botão de voltar e personalização do modal

--- TELA_Comandas/comandas.js
+++ TELA_Comandas/comandas.js
@@ -7,7 +7,7 @@ const headers = {
 }
 
 async function formarComanda(){
-    const comanda = await fetch("https://localhost:7204/api/Comandas",{
+    const comanda = await fetch("https://localhost:7125/api/Comandas",{
         headers:headers,
     })
 
@@ -20,10 +20,11 @@ async function formarComanda(){
 
     Comandas.forEach(async(item,index)=>{
         Ordem.insertAdjacentHTML("beforeend",`
-            
+
         <div class="order"><p><span>${item.id}
         </span><span>MESA ${item.numeroMesa}
         </span></p><span>${item.nomeCliente}</span></div>`)
+        
         
     });
 }
@@ -38,11 +39,14 @@ async function criarComanda(){
     criar.insertAdjacentHTML("beforeend", `
         <div class="wapper">
             <div class="modal">
-                <form id="novaComanda">
-                    <label>Nome</label>
-                    <input type="text">
+                <form id="novaComanda"> 
+                    <button class="close-btn">x</button>
+                    <input id="input_criar_comanda" type="number">
                     <label>Mesa</label>
-                    <input type="number">
+                    <input id="input_criar_comanda" type="text">
+                    <label>Nome</label>
+                   
+                    
                 </form>
                 
                 
@@ -54,6 +58,22 @@ async function criarComanda(){
         </div>`)
 
         formarProduto("#cardapio")
+}
+
+
+const botaoVoltar = document.querySelector(".close-btn")
+if(botaoVoltar){
+        botaoVoltar.addEventListener("click", ()=> {
+         window.location.href ="/home/index.html"}
+)}
+
+const botaoVoltarTelaComanda = document.querySelector(".close-btn_tela_comanda")
+if(botaoVoltarTelaComanda){
+    botaoVoltarTelaComanda.addEventListener("click", ()=> {
+         window.location.href ="/home/index.html"}
+
+)
+
 }
 
 
