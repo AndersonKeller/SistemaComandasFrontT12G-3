7f2355f926168fdba77922d0afe5ae98e151454e
DIOGOGABRIELDOSSANTO <diogosantos2414@gmail.com>
2024-10-11 20:26:40 -0300

    finalizado botao de adicionar e remover itens do cardapio

--- cardapio/formarProduto.js
+++ cardapio/formarProduto.js
@@ -12,6 +12,7 @@ export async function formarProduto(seletor){
     })
     const ItensCardapio = await cardapio.json()
     const items = document.querySelector(seletor)
+    items.innerHTML = ""
     console.log(ItensCardapio,"response await")
     console.log(ItensCardapio)
 
@@ -23,12 +24,43 @@ export async function formarProduto(seletor){
                 <p class="item-name">${item.titulo}</p>
                 <p class="item-desc">${item.descricao}</p>
                 <p class="item-price">R$${item.preco}</p>
-                <button class="editar-item">Editar</button>
+                <button class="editar-item" id=${item.id}>Editar</button>
             </li>
            
         
-            `)}
+            `)
+        
+            const btnEditar = document.getElementById(item.id)
+            btnEditar.addEventListener("click", ()=>{
+                const body = document.querySelector("body")
+
+        body.insertAdjacentHTML("beforeend", `
+            <div class="wapper">
+                <div class="modalNovoCardapio">
+                    <button type="button" class="sairDoCriarItem">X</button>
+                    <p>deseja remover este item?</p>
+                    <button class="removerItem">sim</button>
+            </div>`)  
+            
+
+            const btnSairModalEditar =  document.querySelector(".sairDoCriarItem")
+            btnSairModalEditar.addEventListener("click", ()=>{
+                const modal = document.querySelector(".wapper")
+                modal.remove()
+            })
+
+            const botaoRemover = document.querySelector(".removerItem")
+            botaoRemover.addEventListener("click", ()=>{
+                excluirCardapioItemApi(item.id)
+                const modal = document.querySelector(".wapper")
+                modal.remove()
+            
+            })
+
+            })
+        }
         )
+
     }
 
 
@@ -37,22 +69,67 @@ export async function formarProduto(seletor){
                 botaoVoltar.addEventListener("click", ()=> {
                  window.location.href ="/home/index.html"}
     )
-
         }
         
      
+const btnCriarProduto = document.querySelector(".add")
+btnCriarProduto.addEventListener("click", ()=> {
+    criarProduto
+})
+
+
+
+
 export function criarProduto(){
+    const body = document.querySelector("body")
+
+    body.insertAdjacentHTML("beforeend", `
+        <div class="wapper">
+            <div class="modalNovoCardapio">
+                <form id="formItemCardapio">
+                <h1>Adicionar Novo Item</h1>
+                   <button type="button" class="sairDoCriarItem">X</button>
+                    <label>Nome</label>
+                    <input type="text" id="addItemInput"/>
+                    <label>Preço</label>
+                    <input type="number" id="addItemPrice"/>
+                    <label>Descrição</label>
+                    <input type="text" id="descriptionInput"/>
+                    <button type="submit">Salvar</button>
+                </form>
+            </div>
+                `
+    )   
+    const formNovoProduto = document.querySelector("#formItemCardapio")
+    formNovoProduto.addEventListener("submit",(e)=>{
+        console.log("submit")
+        e.preventDefault()
+        verificarNovoProduto()
+       
+
+    })
+  
+
+    const btnmodal=document.querySelector(".sairDoCriarItem")
+    btnmodal.addEventListener("click",()=>{
+        console.log("click")
+    const modal = document.querySelector(".wapper")
+    modal.remove()
+})
+}
+
+function verificarNovoProduto(){
     const nameInput = document.getElementById('addItemInput');
     const priceInput = document.getElementById('addItemPrice');
-    const descriptionInput = document.getElementById('addItemDescription');
-
+    const descriptionInput = document.getElementById('descriptionInput');
+    console.log(nameInput)
     const name = nameInput.value.trim();
     const price = parseFloat(priceInput.value);
     const description = descriptionInput.value.trim();
 
     if (name && !isNaN(price) && description) {
         const newItem = { titulo: name, preco: price, descricao: description, possuiPreparo:false }
-        items.push(newItem);
+       
     
 
     addCardapioItemApi(newItem)
@@ -65,8 +142,8 @@ export function criarProduto(){
     else {
         alert('Por favor, insira um nome, um preço e uma descrição válidos.');
     }
-    
 }
+
 async function addCardapioItemApi(item){
     const res = await fetch(`${baseUrl}/CardapioItems`,{
         method:"POST",
@@ -76,32 +153,44 @@ async function addCardapioItemApi(item){
     console.log(res)
     const resJson = await res.json()
     console.log(resJson)
+    const modal = document.querySelector(".wapper")
+    modal.remove()
+    formarProduto("#items")
+}
+
+async function excluirCardapioItemApi(id){
+    const res = await fetch(`${baseUrl}/CardapioItems/${id}`,{
+        method:"DELETE",
+        headers:headers
+       
+    })
+    console.log(res)
+    formarProduto("#items")
+   
 }
 
 
-    const body = document.querySelector("body")
 
-        body.insertAdjacentHTML("beforeend", `
-            <div class="wapper">
-                <div class="modalNovoCardapio">
-                    <form id="formItemCardapio">
-                       <button class="sairDoCriarItem">X</button>
-                        <label>Nome</label>
-                        <input type="text" class="nameInput">
-                        <label>Preço</label>
-                        <input type="number" class="nameInput">
-                        <label>Descrição</label>
-                        <input type="text" class="descriptionInput">
-                    </form>
-                </div>
-                    `
-        )    
-    
-    const btnmodal=document.querySelector(".sairDoCriarItem")
-btnmodal.addEventListener("click",()=>{
+
+
+
+
+async function EditarProdutoCardapio(id){
+    const res = await fetch(`${baseUrl}/CardapioItems`,{
+        method:"PUT",
+        headers:headers,
+        body:JSON.stringify(item)
+    })
+    const resJson = await res.json()
     const modal = document.querySelector(".wapper")
     modal.remove()
-})
+    formarProduto("#items")
+}
+
+
+  
+    
+  
     
 
    
