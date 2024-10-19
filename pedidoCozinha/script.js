e29067e2140e53dcc65e0713fd23f593ec1b7498
Artur <07159896181@SCS211077>
2024-10-18 21:06:34 -0300

    alterado o css, index e javascript do pedido cozinha

--- pedidoCozinha/script.js
+++ pedidoCozinha/script.js
@@ -1,42 +1,13 @@
-import {baseUrl} from "../configApi.js"
-
-const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
-// função onde forma a fila de pedidos, fazendo requisão das informações pela API
-async function formarFilaPedidos(element,statusId){
-    const comanda = await fetch(`${baseUrl}/PedidoCozinhas?situacaoId=${statusId}`,{
-        headers:headers,
-    })
-    console.log(comanda)
-   
-    const filaPedidos = await comanda.json()
-    const divSituacaoPedido = document.querySelector(element)
-
-    
-    filaPedidos.forEach(async(item,index)=>{
-        divSituacaoPedido.insertAdjacentHTML("beforeend",`
-        
-            <li>
-                <p class="item-name">Mesa: ${item.numeroMesa}</p>
-                <p class="item-desc">Cliente: ${item.nomeCliente}</p>
-            </li>
-            `)}
-        )
-
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
-// criação da constante botão voltar, onde busca o ID do HTML
-const botaoVoltar = document.getElementById("botao-voltar")
-
-// criação do evento de clique, onde ao clicar volta para a tela home
-botaoVoltar.addEventListener("click", () => {
-    window.location.href ="/home/index.html"
-})
-
-// chamando a função
-formarFilaPedidos(".div-pendentes",1)
-formarFilaPedidos(".div-emAndamento",2)
-formarFilaPedidos(".div-concluido",3)   
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
