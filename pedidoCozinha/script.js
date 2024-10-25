3ddb3324b7b5d43c5acec88021c6007e3571740d
Artur <07159896181@SCS211077>
2024-10-25 19:19:02 -0300

    Criação e alteração da tela pedido Cozinha

--- pedidoCozinha/script.js
+++ pedidoCozinha/script.js
@@ -1,13 +1,54 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+// importando meu ID pessoal da api
+import { baseUrl } from "../configApi.js"
+
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+const botaoVoltar = document.getElementById("voltar")
+botaoVoltar.addEventListener("click", () => {
+    window.location.href ="/home/index.html"
+})
+
+
+// Função para formar os pedidos que vão para a cozinha
+export async function formarPedidos() 
+{
+        
+        // Requisição para buscar os pedidos que vão para cozinha
+        const resposta = await fetch(`${baseUrl}/PedidoCozinhas`, {
+            headers: headers
+        })
+        // Convertendo a resposta para JSON
+        const PedidoCozinhas = await resposta.json()
+        const items = document.getElementById("pendentes")
+        console.log(PedidoCozinhas)
+        PedidoCozinhas.forEach(async (item, index) => {
+            items.insertAdjacentHTML("beforeend", `
+                <li id="modalPedido">
+                    <p class="pedido">Mesa: ${item.numeroMesa} <br> Cliente: ${item.nomeCliente}</p>
+                </li>
+                `)
+        })
+
+        const modalPedido = document.getElementById("modalPedido")
+        modalPedido.addEventListener("click", () => {
+            console.log(items, "item")
+            const body = document.querySelector("body")
+
+            body.insertAdjacentHTML("beforeend", `
+                    <div class="wapper">
+                        <div modal="modalNovoCardapio">
+                            <form id="formItemCardapio">
+                                <h1>teste</h1>
+                            </form>
+                        </div>
+                    </div>
+                
+                `)
+        })
+
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
+// Chama a função 
+formarPedidos()
